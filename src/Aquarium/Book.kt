6c99982920e1013ade9dfeed5c00bd49fff4a47f
Lundberg, Martin (Extern) <Martin.Lundberg.Extern@jsmd-group.com>
2020-03-25 20:52:18 +0100

    collections quiz

--- src/Aquarium/Book.kt
+++ src/Aquarium/Book.kt
@@ -4,6 +4,24 @@ fun main(args: Array<String>) {
     val book = Book("Titel", "Martin", 2010)
 
     println("Here is your book ${book.getTitleAuthor().first} written by ${book.getTitleAuthor().second} in ${book.getTitleAuthorYear().third}")
+
+    val allBooks = setOf(
+       "title1",
+        "title2",
+        "title3"
+    )
+
+    val library = mapOf(
+        "William Shakespeare" to allBooks
+    )
+
+    library.any { it.value.contains("Hamlet")}
+
+    val moreBooks = mutableMapOf(
+        "Harry Potter" to "JK Rowling"
+    )
+
+    moreBooks.getOrPut("Hamlet", {"William Shakespeare"})
 }
 
 class Book(private val title: String, private val author: String, private val year: Int) {
