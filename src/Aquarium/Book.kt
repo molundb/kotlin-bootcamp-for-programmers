b4bbf3d5d0f2c9d59954d07c77b7fd93204be1be
Lundberg, Martin (Extern) <Martin.Lundberg.Extern@jsmd-group.com>
2020-03-28 20:41:31 +0100

    extension functions, quiz

--- src/Aquarium/Book.kt
+++ src/Aquarium/Book.kt
@@ -1,9 +1,11 @@
 package src.Aquarium
 
+import kotlin.random.Random
+
 const val MAXIMUM_NUMBER_OF_BORROWED_BOOKS = 3
 
 fun main(args: Array<String>) {
-    val book = Book("Titel", "Martin", 2010)
+    val book = Book("Titel", "Martin", 2010, 128)
 
     println("Here is your book ${book.getTitleAuthor().first} written by ${book.getTitleAuthor().second} in ${book.getTitleAuthorYear().third}")
 
@@ -24,9 +26,21 @@ fun main(args: Array<String>) {
     )
 
     moreBooks.getOrPut("Hamlet", {"William Shakespeare"})
+
+    val puppy = Puppy()
+
+    while (book.pages > 0) {
+        println(book.pages)
+        puppy.playWithBook(book)
+    }
 }
 
-class Book(private val title: String, private val author: String, private val year: Int) {
+class Book(
+    private val title: String,
+    private val author: String,
+    private val year: Int,
+    var pages: Int
+) {
 
     companion object {
         const val BASE_URL = "www.books.com"
@@ -38,4 +52,13 @@ class Book(private val title: String, private val author: String, private val ye
     fun canBorrow() = MAXIMUM_NUMBER_OF_BORROWED_BOOKS
 
     fun printUrl() = println("$BASE_URL$title.html")
+}
+
+fun Book.getWeight() : Double { return (pages * 1.5) }
+fun Book.tornPages(torn: Int) = if (pages >= torn) pages -= torn else pages = 0
+
+class Puppy() {
+    fun playWithBook(book: Book) {
+        book.tornPages((Random.nextInt(12)))
+    }
 }
