0b2288e4d2cc1991a2600453d009f1f7019dad4c
Lundberg, Martin (Extern) <Martin.Lundberg.Extern@jsmd-group.com>
2020-04-10 15:48:26 +0200

    higher order functions, quiz

--- src/Aquarium5/Aquarium5.kt
+++ src/Aquarium5/Aquarium5.kt
@@ -24,9 +24,24 @@ fun fishExamples() {
         .let { it + "fish" }
         .let { it.length }
         .let { it + 31 })
+
+    val numbers = listOf(1,2,3,4,5,6,7,8,9,0)
+    print(numbers.divisibleBy {
+        it.rem(3)
+    })
 }
 
 fun myWith(name: String, block: String.() -> Unit) {
 
     name.block()
+}
+
+fun List<Int>.divisibleBy(block: (Int) -> Int): List<Int> {
+    val result = mutableListOf<Int>()
+    for (item in this) {
+        if (block(item) == 0) {
+            result.add(item)
+        }
+    }
+    return result
 }
